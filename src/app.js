6adbea9874d5a7096487d95f9e3220e9c8120d56
tharaka10 <truwanmaduwantha@gmail.com>
2026-02-12 23:31:40 +0530

    Secure our API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(securityMiddleware);
 
 app.use(morgan('combined', {
   stream: {
