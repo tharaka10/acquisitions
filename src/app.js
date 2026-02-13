2c5e8e2dfa54dbcc4c6660a235944f3c1eca6547
tharaka10 <truwanmaduwantha@gmail.com>
2026-02-14 00:01:39 +0530

    Implement Testing

--- src/app.js
+++ src/app.js
@@ -39,4 +39,8 @@ app.get('/api', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route Not Found'});
+})
+
 export default app;
